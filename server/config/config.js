ec66ea808dffcd5f43d8122eb1f66fb4e75fd253
BlackWizarz <dngrowth01@gmail.com>
2019-02-17 11:48:46 +0700

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,12 @@
 let env = process.env.NODE_ENV || 'development'
 console.log('env ********', env)
 
-if (env === 'development') {
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp"
-} else if (env === 'test') {
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest"
-} else if (env === 'production') {
-    process.env.MONGODB_URI = "mongodb://whitebnb:blacknbn1@cluster0-shard-00-00-tmsuu.mongodb.net:27017,cluster0-shard-00-01-tmsuu.mongodb.net:27017,cluster0-shard-00-02-tmsuu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+if (env === 'development' || env === 'test') {
+    let config = require('./config.json')
+    let envConfig = config[env]
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key]
+    })
 }
+
